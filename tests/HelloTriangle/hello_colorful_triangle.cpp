5e747d0787adcbd96f117102b4d3379b569357e6
The-Minecraft-Scientist <The-Minecraft-Scientist@users.noreply.github.com>
2024-09-25 17:20:57 -0700

    switch the test back from nsgl_shim shenanigans

--- tests/HelloTriangle/hello_colorful_triangle.cpp
+++ tests/HelloTriangle/hello_colorful_triangle.cpp
@@ -23,7 +23,7 @@ int main() {
   // ------------------------------
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // glfw window creation
